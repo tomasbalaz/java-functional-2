ffcfa8f3b6d5f31fb9c80ef6a22df1ddd837458f
Tomas Balaz <balaztomas0@gmail.com>
2023-08-18 15:25:21 +0200

    Created CustomerRegistrationValidator as combinator pattern via functional style

---
+++ src/main/java/combinatornpattern/CustomerRegistrationValidator.java
@@ -0,0 +1,42 @@
+package combinatornpattern;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Function;
+
+import static combinatornpattern.CustomerRegistrationValidator.*;
+import static combinatornpattern.CustomerRegistrationValidator.ValidationResult.*;
+
+public interface CustomerRegistrationValidator extends Function<Customer, ValidationResult> {
+
+    static CustomerRegistrationValidator isEmailValid() {
+        return customer -> customer.email().contains("@") ?
+                SUCCESS: EMAIL_NOT_VALID;
+    }
+
+    static CustomerRegistrationValidator isPhoneNumberValid() {
+        return customer -> customer.phoneNumber().startsWith("+0") ?
+                SUCCESS: PHONE_NUMBER_NOT_VALID;
+    }
+
+    static CustomerRegistrationValidator isAdult() {
+        return customer -> Period.between(customer.dob(), LocalDate.now()).getYears() > 16 ?
+                SUCCESS: IS_NOT_AN_ADULT;
+    }
+
+    default CustomerRegistrationValidator and (CustomerRegistrationValidator other) {
+        return customer -> {
+            ValidationResult result = this.apply(customer);
+            return result.equals(SUCCESS) ?
+                    other.apply(customer) : result;
+
+        };
+    }
+
+    enum ValidationResult {
+        SUCCESS,
+        PHONE_NUMBER_NOT_VALID,
+        EMAIL_NOT_VALID,
+        IS_NOT_AN_ADULT
+    }
+}
--- src/main/java/combinatornpattern/Main.java
+++ src/main/java/combinatornpattern/Main.java
@@ -2,6 +2,8 @@ package combinatornpattern;
 
 import java.time.LocalDate;
 
+import static combinatornpattern.CustomerRegistrationValidator.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,6 +15,20 @@ public class Main {
         );
 
         System.out.println(new CustomerValidatorService().isCustomerValid(customer));
+
+        //Using combinator pattern
+
+        ValidationResult result = isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAdult())
+                .apply(customer);
+
+
+        if(result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+
+        }
+        System.out.println(result);
 
         //if valid we can store customer in DB.
     }
